1a61f7eae342a779f13d5401c611702d8c711eee
Atul Sahu <sahuatul2001@gmail.com>
2024-02-15 13:18:16 +0530

    complete Weather app

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ async function fetchWeatherData(cityName){
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=58f2f45e00cb1cf47fbb8ddbf0452a6d`;
 	const response =await fetch(url)
 	const data =await response.json();
-	console.log(data)
+	let city =document.getElementById("city");
+	city.innerHTML = data.name;
+	let windSpeed=document.getElementById("wind_speed");
+	windSpeed.innerHTML=data.wind.speed + "km/hr";
+	let humidity= document.getElementById("humidity");
+	humidity.innerHTML= data.main.humidity +"g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
+	console.log(data);
  
 }
 function fetchCity()
